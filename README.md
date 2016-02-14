6bfdba1d8e0b78e17bdd0cfcab5822e2cbd4fae5
smooshi <timereh@gmail.com>
2016-02-14 20:20:25 +0200

    w4 complete

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ wepa
 http://gentle-coast-91732.herokuapp.com/
 
 [![Build Status](https://travis-ci.org/smooshi/ratebeer.png)](https://travis-ci.org/smooshi/ratebeer)
+
+[![Code Climate](https://codeclimate.com/github/smooshi/ratebeer.png)](https://codeclimate.com/github/smooshi/ratebeer)
--- spec/features/beer_spec.rb
+++ spec/features/beer_spec.rb
@@ -5,8 +5,10 @@ include Helpers
 describe "Beer" do
   let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
   let!(:beer1) { FactoryGirl.create :beer, name:"iso 3", brewery:brewery }
+  let!(:user) { FactoryGirl.create :user }
 
   it "create beer with valid name" do
+    sign_in(username:"Pekka", password:"Foobar1")
     visit new_beer_path
     fill_in('beer_name', with:"HienoOlut")
     click_button('Create Beer')
